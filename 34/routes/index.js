59e18c1bca8e58b34a7835451f7b7c0f4ef24745
thai.duc <huynhjduc248@gmail.com>
2018-12-05 23:00:05 +0700

    'session'

--- 34/routes/index.js
+++ 34/routes/index.js
@@ -2,17 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/detail/:id',function (req,res,next) {
-  var id = req.params.id;
-  res.render('detail',{id:id});
+router.get('/detail/:id', function (req, res, next) {
+    var id = req.params.id;
+    if (!req.session.viewed) {
+        req.session.viewed = [];
+    }
+    req.session.viewed.push(id);
+    res.render('detail', {id: id});
 });
-router.get('/view',function (req,res,next) {
-
+router.get('/viewed', function (req, res, next) {
+    res.render('viewed',{ zxc : req.session.viewed });
 });
 
 module.exports = router;
